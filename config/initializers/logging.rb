185e2dcf3fc27838f3274fec930bddfa2a29e88c
Darby Frey <darbyfrey@gmail.com>
2014-08-22 23:56:54 -0500

    logging

--- config/initializers/logging.rb
+++ config/initializers/logging.rb
@@ -1,6 +1,37 @@
-Logging.appenders.reset
+module Napa
+  class Logger
+    class << self
+      def name
+        [Napa::Identity.name, Napa::LogTransaction.id].join('-')
+      end
 
-Logging.appenders.stdout(
-  'stdout',
-  layout: Logging.layouts.json
-)
+      def logger=(logger)
+        @logger = logger
+      end
+
+      def logger
+        unless @logger
+          Logging.appenders.stdout(
+            'stdout',
+            layout: Logging.layouts.json
+          )
+
+          @logger = Logging.logger["[#{name}]"]
+          @logger.add_appenders 'stdout' unless Napa.env.test?
+        end
+
+        @logger
+      end
+
+      def response(status, headers, body)
+        { response:
+          {
+            status:   status,
+            headers:  headers,
+            response: body
+          }
+        }
+      end
+    end
+  end
+end
